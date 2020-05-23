c30752e4549a38d0c60dcc554ec702a65c42103c
tinajenssen <63742433+tinajenssen@users.noreply.github.com>
2020-05-23 11:29:49 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,38 @@
+// question 1
+
+// var firstVar="";
+
+// question 2
+
+// var name="Tina";
+
+// question 3
+
+// var year= 2020;
+
+// question 4
+
+// var division=20/5;
+
+// question 5
+
+// var animal = "frog";
+// typeof (animal);
+
+// question 6
+
+// orderHasShipped=true;
+
+// question 7
+
+// if(orderHasShipped === true){
+//   console.log("True");
+// } else {
+//   console.log("false");
+// }
+
+// question 8
+
+// for(var count = 1; count <= 9; count++) {
+//   console.log(count);
+// }
